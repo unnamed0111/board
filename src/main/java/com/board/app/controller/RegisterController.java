4a692b80a3f0c0f89c9fef7888d2a1adf4b4e0b7
Cat Hanbit <hsccomsen@gmail.com>
2022-07-09 02:20:59 +0900

    Validation 추가

--- src/main/java/com/board/app/controller/RegisterController.java
+++ src/main/java/com/board/app/controller/RegisterController.java
@@ -2,16 +2,19 @@ package com.board.app.controller;
 
 import com.board.app.dao.UserDao;
 import com.board.app.domain.User;
+import com.board.app.domain.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -27,7 +30,8 @@ public class RegisterController {
     @InitBinder
     public void toDate(WebDataBinder binder) {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(df, false));
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(df, true));
+        binder.setValidator(new UserValidator());
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
@@ -36,8 +40,18 @@ public class RegisterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String register(User user) throws Exception {
+    public String register(@Valid User user, BindingResult result) throws Exception {
         System.out.println("user = " + user);
+
+        // 아이디 중복 확인
+        User userExisted = userDao.selectUser(user.getUserId());
+
+        if(userExisted != null) {
+            result.rejectValue("userId", "exist");
+        }
+
+        if(result.hasErrors()) return "register";
+
         userDao.insertUser(user);
         return "redirect:/";
     }
